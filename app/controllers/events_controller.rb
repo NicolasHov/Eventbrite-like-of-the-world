fee5a084bb94884363e638b5c0c73b5de7cf9ba5
Nicolas Hovart <kola.hov@gmail.com>
2018-11-10 15:43:21 +0100

    Index, New, Show, Update

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,38 @@
 class EventsController < ApplicationController
-  def home
-  end
+
 
   def index
     @events = Event.all 
   end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+  
+  def update
+    @event = Event.find(params[:id])
+    @event.update(post_params)
+    redirect_to event_path(@event.id)
+  end
+
+  def new
+    @event = Event.new
+  end
+
+
+  def create
+    event = Event.create(post_params)
+    redirect_to edit_event_path(event.id)
+  end
+
+  private 
+
+  def post_params
+    params.require(:event).permit(:name, :description, :place)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,4 @@
 a= Event.create(description:"Le kiff ultime", name:"Atelier Kiff", place:"Quelquepart de bien 12 rue de l'olivier Paris Cedex")
 b= Event.create(description:"Pas le kiff ultime", name:"Atelier Pas kiff", place:"Quelquepart de pas bien 14 bvd du loinloin Tresloin Cedex") 
 c= Event.create(description:"Viens faire des révélations", name:"Atelier Alleluia", place:"Quelquepart de pas bien 14 bvd du loinloin Tresloin Cedex")
+d= Event.create(name:"Test", description:"bakbalaboa", place:"Labas cest pas loin frere")
