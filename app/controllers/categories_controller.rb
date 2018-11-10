f49b88914220d1079cb2860526c0dfe87d90a9ee
Nicolas Hovart <kola.hov@gmail.com>
2018-11-10 19:57:39 +0100

    WIP Add model Category & controller Categoories

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,47 @@
+class CategoriesController < ApplicationController
+
+
+  def index
+    @category = Category.all 
+  end
+
+  def show
+    @category= Category.find(params[:id])
+  end
+
+  def new
+    @category= Category.new
+  end
+
+  def update
+    @category= Category.find(params[:id])
+    if @category.update(post_params)
+      redirect_to category_path(@category.id)
+    else 
+      render "edit"
+    end
+  end
+
+  def edit
+    @category= Category.find(params[:id])
+  end
+
+  #ajouter le if else
+  def create
+    @category= Category.create(post_params)
+    redirect_to edit_category_path(category.id)
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to categories_path
+  end
+
+  private 
+
+  def post_params
+    params.require(:category.permit(:name, :description, :place))
+  end
+
+end
